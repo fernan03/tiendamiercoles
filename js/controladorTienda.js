3b95177c8f3456e3e940388537a4b1c2f4135ab8
203 <203@academicos.cesde.edu.co>
2022-02-16 10:04:50 -0500

    se crea controlador

---
+++ js/controladorTienda.js
@@ -0,0 +1,26 @@
+// Entrada de problema
+let nombreProducto ="batimovil"
+let precioProducto ="400000"
+let pesoProducto = 18.5
+let tieneDescuento = true
+let descripcionProducto ="Batimovil a escala"
+let telefonoVendedor ="1234567"
+
+const NOMBRE_CLIENTE="Luis"
+
+// Arreglos unidemencionales
+// Sirven para almacenar varios elementos en una sola variables
+let productos=["batimovil","camiseta","gorra","bati morral"]
+let productos2=Array("batimovil","camiseta","gorra","bati morral")
+
+
+//Salidas
+console.log("Buenas tardes")
+console.log(nombreProducto)
+console.log("Su nombre es: "+NOMBRE_CLIENTE+ " y su producto es: "+nombreProducto)
+console.log(`Su nombre es: ${NOMBRE_CLIENTE} y su producto es: ${nombreProducto}`)
+console.log("************")
+console.log(productos)
+console.log(productos2[2])
+console.log(productos2[3])
+console.log(productos2[1])
